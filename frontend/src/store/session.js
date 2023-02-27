e6650274658e330d770d7cc3edf9156680f3724c
AJ Glickauf <ajglick2000@gmail.com>
2023-02-27 16:34:54 -0600

    SignupFormPage

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -10,6 +10,7 @@ const setUser = (user) => {
     };
 };
 
+// eslint-disable-next-line
 const removeUser = () => {
     return {
         type: REMOVE_USER,
@@ -32,6 +33,23 @@ export const login = (user) => async (dispatch) => {
 
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
